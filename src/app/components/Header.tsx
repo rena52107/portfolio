1e333793133daa6974d149b979c84a6215d03ecb
Rena Sakata <rena.sakata.cn@gmail.com>
2024-02-14 11:43:51 -0800

    add elements

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,25 @@
-import React from "react";
-
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex flex-row justify-between">
+      <a href="/">Logo</a>
+      <nav>
+        <ul className="flex flex-row gap-10">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#works">Works</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
