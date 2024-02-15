404934e4df5d26b882df2bc3ce1255155880a860
Rena Sakata <rena.sakata.cn@gmail.com>
2024-02-15 14:01:56 -0800

    add basic ele

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="wrapper bg-primary-dark text-white">
+      <footer className="flex justify-between">
+        <p>© Rena Sakata 2024</p>
+        <nav>
+          <ul className="flex flex-row gap-10">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#works">Works</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  );
 }
 
 export default Footer;
--- src/app/components/Outro.tsx
+++ src/app/components/Outro.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Contact from "./Contact";
 
 function Outro() {
-  return <div>Outro</div>;
+  return (
+    <section>
+      <h2>Thanks for checking out my portfolio</h2>
+      <p>
+        Feel free to reach out to me on any of the platforms below!
+        <br /> I&apos;d love to hear from you ;)
+      </p>
+      <Contact />
+    </section>
+  );
 }
 
 export default Outro;
