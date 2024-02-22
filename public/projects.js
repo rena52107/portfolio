374cb71e102ac686bd187a68f25bce17df46acf0
Rena Sakata <rena.sakata.cn@gmail.com>
2024-02-21 16:44:45 -0800

    add project data

--- public/projects.js
+++ public/projects.js
@@ -1,6 +1,26 @@
 export const projects = [
   {
     id: 1,
+    title: "DailyGo",
+    tag: "team",
+    description:
+      "A mobile app that promotes daily physical activity through gamified challenges. Users set step goals and walk or run individually or as a team to collect fireflies and cheer up in-app monsters. Main features include a walking tracker for real-time progress monitoring, gamification with firefly collection, streak rewards for consistent activity, and collaboration with other users through group challenges and social sharing.",
+    techStack: [
+      "React Native",
+      "Firebase",
+      "AWS",
+      "JWT",
+      "MongoDB",
+      "Lottie",
+      "Node.js",
+      "Express",
+    ],
+    imageUrl: "dailygo_demo.gif",
+    githubUrl: "https://github.com/Lion-teambits/dailyGo-app",
+    buttonInfo: { title: "Marketing site", url: "https://dailygo.ca" },
+  },
+  {
+    id: 2,
     title: "Edspresso",
     tag: "team",
     description:
@@ -16,6 +36,36 @@ export const projects = [
     ],
     imageUrl: "edspresso_trainee.gif",
     githubUrl: "https://github.com/Supernova-teambits/edspresso-supernova",
-    buttonInfo: { title: "Demo", url: "https://edspresso.ca/userTestlogin" },
+    buttonInfo: {
+      title: "Live Demo",
+      url: "https://edspresso.ca/userTestlogin",
+    },
+  },
+  {
+    id: 3,
+    title: "WheelScout",
+    tag: "team",
+    description:
+      "An accessibility reporting site that offers live updates and hotspot overviews of nearby destinations for users of mobility aids, streamlining journey planning with the latest community reports. Created at Langara Hackathon 2023.",
+    techStack: [
+      "React",
+      "Google Maps API",
+      "JSON Server",
+      "Node.js",
+      "Vite",
+      "CORS",
+      "Material UI",
+    ],
+    imageUrl: "wheelScout_demo.gif",
+    githubUrl: "https://github.com/rena52107/hackaton-team-wmdd",
+  },
+  {
+    id: 4,
+    title: "BeWear",
+    tag: "team",
+    description:
+      "A weather-based outfit suggestion app (PWA) that helps users decide what to wear based on their temperature sensitivity and the weather forecast. It covers all weather details throughout the day and suggests outfits for morning, evening, and nighttime.",
+    techStack: ["HTML5", "Sass", "jQuery", "JavaScript", "Firebase"],
+    imageUrl: "bewear_demo.gif",
   },
 ];
