5eb61eeb26af0b1c362532dacbbb2254a7988e5e
Rena Sakata <rena.sakata.cn@gmail.com>
2024-02-23 12:41:15 -0800

    style: Change content

--- src/app/components/Outro.tsx
+++ src/app/components/Outro.tsx
@@ -9,12 +9,12 @@ function Outro() {
         className="flex flex-col justify-center items-center md:mx-auto md:max-w-130 gap-10"
       >
         <h2 className="text-center md:text-2xl text-heading4">
-          Thank you for reading
+          Thank you for taking the time to explore my work :)
         </h2>
         <p className="text-center md:text-body1">
-          Feel free to reach out to me!
+          If you have any questions or would like to collaborate,
           <br />
-          I&apos;d love to hear from you ;)
+          please feel free to contact me!
         </p>
         <Contact />
       </section>
