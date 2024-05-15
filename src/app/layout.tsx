e50b1a3eceedefe6d391c485e575e973a835330f
Rena Sakata <rena.sakata.cn@gmail.com>
2024-05-15 09:45:23 -0700

    feat: Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import "./styles/globals.css";
 import Header from "./components/Header";
@@ -8,7 +9,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Rena Sakata - Full Stack Developer",
-  description: "As a goal-oriented developer specializing in full-stack development, I'm committed to creating accessible and innovative solutions",
+  description:
+    "As a goal-oriented developer specializing in full-stack development, I'm committed to creating accessible and innovative solutions",
 };
 
 export default function RootLayout({
@@ -18,6 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-C8S0K6MJK1" />
       <body className={`${inter.className} text-bg-dark bg-bg-light font-sans`}>
         <Header />
         {children}
